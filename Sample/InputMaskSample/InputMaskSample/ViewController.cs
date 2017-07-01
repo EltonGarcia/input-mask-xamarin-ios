6c71f03f14e4984bf6e1bc6128845816d4df1272
Elton Garcia <elton.garcia.s@gmail.com>
2017-07-01 00:22:22 -0300

    Sample added

---
+++ Sample/InputMaskSample/InputMaskSample/ViewController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using Foundation;
+using InputMask.Classes.View;
+using UIKit;
+
+namespace InputMaskSample
+{
+    public partial class ViewController : UIViewController, IMaskedTextFieldDelegateListener
+    {
+        MaskedTextFieldDelegate maskedDelegate;
+
+        protected ViewController(IntPtr handle) : base(handle) { }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            maskedDelegate = new MaskedTextFieldDelegate("8 ([000]) [000] [00] [00]");
+            maskedDelegate.Listener = this;
+
+            field.Delegate = maskedDelegate;
+        }
+
+        public void TextField(IUITextInput textField, bool complete, string value)
+        {
+            Debug.WriteLine(value);
+        }
+
+        [Export("textField:shouldChangeCharactersInRange:replacementString:")]
+        public bool ShouldChangeCharacters(UITextField textField, Foundation.NSRange range, string replacementString)
+        {
+            Debug.WriteLine(textField.Text);
+            return true;
+        }
+    }
+}
